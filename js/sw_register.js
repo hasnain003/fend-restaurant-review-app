7d00b4caa2443246003d2dc4e0bc69e650b7157a
hasnain003 <mdhasnain839@gmail.com>
2018-11-23 23:36:04 +0530

    Change if statement

--- js/sw_register.js
+++ js/sw_register.js
@@ -1,7 +1,7 @@
-if(!navigator.serviceWorker) return;
-
-navigator.serviceWorker.register('./sw.js').then(function() {
-	console.log("Registration Worked");
-}).catch(function() {
-	console.log("Registration Failed!");
-});
+if(navigator.serviceWorker) {
+	navigator.serviceWorker.register('./sw.js').then(function() {
+		console.log("Registration Worked");
+	}).catch(function() {
+		console.log("Registration Failed!");
+	});
+}
